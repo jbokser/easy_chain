39170a1a4f1465d2e7cbfd1517c6ba9adb0c0d84
Juan S. Bokser <juan.bokser@gmail.com>
2020-10-18 14:30:37 -0300

    add example private key from env

---
+++ examples/private_key_from_env.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from easy_chain.wallet import Wallet
+
+# Non persistent wallet
+wallet = Wallet(use_env="PKEY_") 
+
+
+def main():
+
+    if not 'TEST' in wallet:
+        print('')
+        print('You must use the environment variable PKEY_TEST')
+        print('with the value of a private key.')
+        print('')
+        print('for example:')
+        print('')
+        print('PKEY_TEST=0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d ./private_key_from_env.py')
+        print('')
+        return
+
+    address = wallet['TEST']
+
+    print('')
+    print(address)
+    print('')
+
+
+
+if __name__ == '__main__':
+    main()
