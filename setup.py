c1e087bf61cec55ea3fd072fd1d6aa647f8aa876
Juan S. Bokser <juan.bokser@gmail.com>
2020-10-20 21:06:59 -0300

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file_:
 
 setup(
     name='easy_chain',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     author='Juan S. Bokser',
     author_email='juan.bokser@gmail.com',
